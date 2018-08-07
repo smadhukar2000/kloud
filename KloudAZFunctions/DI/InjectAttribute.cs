e768f6228252dd010c46e2a8b77a5ff092aa607f
Subhash Madhukar <subhash.madhukar@team.telstra.com>
2018-08-07 17:57:31 +1000

    added DI support in azure functions

---
+++ KloudAZFunctions/DI/InjectAttribute.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Azure.WebJobs.Description;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KloudAZFunctions.DI
+{
+    [Binding]
+    [AttributeUsage(AttributeTargets.Parameter, AllowMultiple = false)]
+    public class InjectAttribute : Attribute
+    {
+        public InjectAttribute(Type type)
+        {
+            Type = type;
+        }
+        public Type Type { get; }
+    }
+}
