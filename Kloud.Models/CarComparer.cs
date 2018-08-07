46bb5cb7d34e23e2dfc54d6a37db20da1687472f
Subhash Madhukar <subhash.madhukar@team.telstra.com>
2018-08-07 16:16:41 +1000

    added unit test cases with MOQ

---
+++ Kloud.Models/CarComparer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kloud.Models
+{
+    public class CarComparer : EqualityComparer<Car>
+    {
+        public override bool Equals(Car x, Car y)
+        {
+            if (string.Compare(x.Brand, y.Brand) == 0 && string.Compare(x.Colour, y.Colour) == 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public override int GetHashCode(Car obj)
+        {
+            return 0;
+        }
+    }
+}
